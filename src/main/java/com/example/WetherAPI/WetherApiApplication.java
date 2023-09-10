405d5159f50a2d952fbf23997f6fd29a19360339
Faizan2901 <mo.faizansopariwala2901@gmail.com>
2023-09-10 21:26:13 +0530

    Project of RESTAPI of WhetherAPI is added

---
+++ src/main/java/com/example/WetherAPI/WetherApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.WetherAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WetherApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WetherApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/WetherAPI/WetherApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WetherAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WetherApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
