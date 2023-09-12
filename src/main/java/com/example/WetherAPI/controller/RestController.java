6d450d9b41c65ae927f4e120f1bd329c95d186c8
Faizan2901 <mo.faizansopariwala2901@gmail.com>
2023-09-12 22:40:45 +0530

    Mapping is added for show home page.

--- src/main/java/com/example/WetherAPI/controller/RestController.java
+++ src/main/java/com/example/WetherAPI/controller/RestController.java
@@ -1,13 +1,17 @@
 package com.example.WetherAPI.controller;
 
+import com.example.WetherAPI.bean.WeatherData;
 import com.example.WetherAPI.service.WebServiceClientImlp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 @RequestMapping("/whether")
@@ -16,9 +20,16 @@ public class RestController {
     @Autowired
     WebServiceClientImlp webServiceClientImlp;
 
-    @GetMapping("/api")
-    private String getWhether() throws URISyntaxException, IOException {
-        webServiceClientImlp.getWhether("London");
+    @GetMapping("/home")
+    private String getHomePage(Model model){
+        List<String> cities= Arrays.asList("Delhi", "Mumbai", "Kolkata", "Chennai", "Bengaluru", "Hyderabad");
+        model.addAttribute("cities",cities);
+        return "home-page";
+    }
+    @PostMapping("/api")
+    private String getWhether(@RequestParam("selectedCity") String city) throws Exception {
+        WeatherData weather=webServiceClientImlp.getWhether(city);
+        System.out.println(weather.getMain().getTemp());
         return "<h1>Hello</h1>";
     }
 
