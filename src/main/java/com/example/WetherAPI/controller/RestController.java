09ff9c52827544a327fe8baeb33bec3b1b907b16
Faizan2901 <mo.faizansopariwala2901@gmail.com>
2023-09-13 14:25:00 +0530

    Add return page to dashboard.

--- src/main/java/com/example/WetherAPI/controller/RestController.java
+++ src/main/java/com/example/WetherAPI/controller/RestController.java
@@ -10,11 +10,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 @Controller
-@RequestMapping("/whether")
+@RequestMapping("/weather")
 public class RestController {
 
     @Autowired
@@ -22,15 +25,24 @@ public class RestController {
 
     @GetMapping("/home")
     private String getHomePage(Model model){
-        List<String> cities= Arrays.asList("Delhi", "Mumbai", "Kolkata", "Chennai", "Bengaluru", "Hyderabad","Ahmedabad");
+        List<String> cities= Arrays.asList("Delhi", "Mumbai", "Kolkata", "Chennai", "Bengaluru", "Hyderabad","Ahmedabad","Pune");
         model.addAttribute("cities",cities);
         return "home-page";
     }
     @PostMapping("/api")
-    private String getWhether(@RequestParam("selectedCity") String city) throws Exception {
+    private String getWhether(@RequestParam("selectedCity") String city,Model model) throws Exception {
         WeatherData weather=webServiceClientImlp.getWhether(city);
         System.out.println(weather.getTemp());
-        return "redirect:/whether/home";
+
+        SimpleDateFormat dateFormat=new SimpleDateFormat("dd-M-yyyy hh:mm:ss");
+        Date date=new Date();
+        String dateString = dateFormat.format(date);
+
+        model.addAttribute("dateString",dateString);
+        model.addAttribute("city",city);
+        model.addAttribute("weather",weather);
+
+        return "dash-board";
     }
 
 }
