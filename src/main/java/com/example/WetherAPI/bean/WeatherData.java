4cc80ed859cc7bcefdf08c1d8a5ddb83483a257a
Faizan2901 <mo.faizansopariwala2901@gmail.com>
2023-09-13 14:25:26 +0530

    New element is added.

--- src/main/java/com/example/WetherAPI/bean/WeatherData.java
+++ src/main/java/com/example/WetherAPI/bean/WeatherData.java
@@ -1,63 +1,83 @@
 package com.example.WetherAPI.bean;
+
 import java.util.List;
 
 public class WeatherData {
 
-        private double temp;
-        private double feels_like;
-        private double temp_min;
-        private double temp_max;
-        private int pressure;
-        private int humidity;
+    private double temp;
+    private double feels_like;
+    private double temp_min;
+    private double temp_max;
+    private int pressure;
+    private int humidity;
 
-        public double getTemp() {
-            return temp;
-        }
+    private int grnd_level;
 
-        public void setTemp(double temp) {
-            this.temp = temp;
-        }
+    private int sea_level;
 
-        public double getFeels_like() {
-            return feels_like;
-        }
+    public int getGrnd_level() {
+        return grnd_level;
+    }
 
-        public void setFeels_like(double feels_like) {
-            this.feels_like = feels_like;
-        }
+    public void setGrnd_level(int grnd_level) {
+        this.grnd_level = grnd_level;
+    }
 
-        public double getTemp_min() {
-            return temp_min;
-        }
+    public double getTemp() {
+        return temp;
+    }
 
-        public void setTemp_min(double temp_min) {
-            this.temp_min = temp_min;
-        }
+    public void setTemp(double temp) {
+        this.temp = temp;
+    }
 
-        public double getTemp_max() {
-            return temp_max;
-        }
+    public double getFeels_like() {
+        return feels_like;
+    }
 
-        public void setTemp_max(double temp_max) {
-            this.temp_max = temp_max;
-        }
+    public void setFeels_like(double feels_like) {
+        this.feels_like = feels_like;
+    }
 
-        public int getPressure() {
-            return pressure;
-        }
+    public double getTemp_min() {
+        return temp_min;
+    }
 
-        public void setPressure(int pressure) {
-            this.pressure = pressure;
-        }
+    public void setTemp_min(double temp_min) {
+        this.temp_min = temp_min;
+    }
 
-        public int getHumidity() {
-            return humidity;
-        }
+    public double getTemp_max() {
+        return temp_max;
+    }
 
-        public void setHumidity(int humidity) {
-            this.humidity = humidity;
-        }
+    public void setTemp_max(double temp_max) {
+        this.temp_max = temp_max;
+    }
 
+    public int getPressure() {
+        return pressure;
+    }
+
+    public void setPressure(int pressure) {
+        this.pressure = pressure;
+    }
+
+    public int getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(int humidity) {
+        this.humidity = humidity;
+    }
+
+    public int getSea_level() {
+        return sea_level;
+    }
+
+    public void setSea_level(int sea_level) {
+        this.sea_level = sea_level;
+    }
 }
 
 
